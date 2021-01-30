ebc51b14843847b9b8c032e70de8fe9578ba65bc
dequevedo <danielelvisquevedo@gmail.com>
2021-01-30 16:04:58 -0300

    Dynamic port

--- index.js
+++ index.js
@@ -34,6 +34,6 @@ wss.on('connection', function(ws) {
   });
 });
 
-server.listen(8080, function() {
-  console.log('Listening on http://localhost:8080');
+server.listen(process.env.PORT || 8080, function() {
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
